197482d030b024f09ff9f7f038a11f35cb8021fd
Egor Tensin <Egor.Tensin@gmail.com>
2017-04-16 01:39:30 +0300

    backup_gitconfig.sh -> configure_git.sh

--- configure_git.sh
+++ configure_git.sh
@@ -2,5 +2,6 @@
 
 set -o errexit -o nounset -o pipefail
 
-script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )"
+script_dir="$( dirname -- "${BASH_SOURCE[0]}" )"
+script_dir="$( cd -- "$script_dir" && pwd )"
 cp --remove-destination -- "$script_dir/%HOME%/.gitconfig" ~
