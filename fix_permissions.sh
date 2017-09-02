8b0e9bd7058c9e1c4929f46c436cee659648a350
Egor Tensin <Egor.Tensin@gmail.com>
2017-09-02 22:32:09 +0300

    fix_permissions.sh: exclude .git/ properly

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -4,4 +4,4 @@ set -o errexit -o nounset -o pipefail
 
 script_dir="$( dirname -- "${BASH_SOURCE[0]}" )"
 script_dir="$( cd -- "$script_dir" && pwd )"
-find "$script_dir" -mindepth 1 -path ./.git -prune -o -exec chmod g-w {} +
+find "$script_dir" -mindepth 1 -path "$script_dir/.git" -prune -o -exec chmod g-w {} +
