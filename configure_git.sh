fb2b6e57cc24a8182ef8ea3c25bcb664e9286acd
Egor Tensin <Egor.Tensin@gmail.com>
2017-09-22 15:46:26 +0300

    remove configure_git.sh
    What was it for, anyway?

--- configure_git.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit -o nounset -o pipefail
-
-script_dir="$( dirname -- "${BASH_SOURCE[0]}" )"
-script_dir="$( cd -- "$script_dir" && pwd )"
-cp --remove-destination -- "$script_dir/%HOME%/.gitconfig" ~
