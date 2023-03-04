532dcde6800f59cc027f5d6f22565a889567630c
Egor Tensin <Egor.Tensin@gmail.com>
2023-03-04 10:10:45 +0100

    fix_permissions.sh: fix

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 # Some utilities (for example, ssh) fix too relaxed file permissions
-# automatically.
-# Others (GHCi is the reason this script exists) just spit out a warning and
-# ignore such files.
-# This script simply removes the write permission for everybody except myself
-# for every file in this repository.
+# automatically. Others (GHCi is the reason this script exists) just spit out a
+# warning and ignore such files. This script simply removes the write
+# permission for everybody except myself for every file in this repository.
 
 set -o errexit -o nounset -o pipefail
 shopt -s inherit_errexit lastpipe
@@ -13,5 +11,8 @@ shopt -s inherit_errexit lastpipe
 script_dir="$( dirname -- "${BASH_SOURCE[0]}" )"
 script_dir="$( cd -- "$script_dir" && pwd )"
 readonly script_dir
+
+cd -- "$script_dir"
+export PATH="$script_dir/../config-links:$PATH"
 
 links-chmod go-w
